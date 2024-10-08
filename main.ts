873ca210313746a60647e715f0e2f0ab3a53b4d3
ella-an2 <ella.an@mths.ca>
2024-10-08 11:21:29 -0400

    started to program

--- main.ts
+++ main.ts
@@ -1,8 +1,17 @@
 /* Copyright (c) 2020 MTHS All rights reserved
  *
- * Created by: Mr. Coxall
- * Created on: Sep 2020
- * This program ...
+ * Created by: Ella
+ * Created on: Oct 2024
+ * This program is a traffic light
 */
 
-basic.showString('Hello, World!')
+let neopixelLight: neopixel.Strip = null
+
+basic.clearScreen()
+neopixelLight = neopixel.create(DigitalPin.P16, 4, NeoPixelMode.RGB)
+neopixelLight.setPixelColor(0, neopixel.colors(NeoPixelColors.Black))
+neopixelLight.setPixelColor(1, neopixel.colors(NeoPixelColors.Black))
+neopixelLight.setPixelColor(2, neopixel.colors(NeoPixelColors.Black))
+neopixelLight.show()
+basic.showIcon(IconNames.Happy)
+
