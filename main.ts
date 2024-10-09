2b61ab89f9940e77a392960cfae060d7d9e1a3cb
ella-an2 <ella.an@mths.ca>
2024-10-09 19:33:23 -0400

    Programmed the code

--- main.ts
+++ main.ts
@@ -9,9 +9,29 @@ let neopixelLight: neopixel.Strip = null
 
 basic.clearScreen()
 neopixelLight = neopixel.create(DigitalPin.P16, 4, NeoPixelMode.RGB)
-neopixelLight.setPixelColor(0, neopixel.colors(NeoPixelColors.Black))
-neopixelLight.setPixelColor(1, neopixel.colors(NeoPixelColors.Black))
 neopixelLight.setPixelColor(2, neopixel.colors(NeoPixelColors.Black))
+neopixelLight.setPixelColor(1, neopixel.colors(NeoPixelColors.Black))
+neopixelLight.setPixelColor(0, neopixel.colors(NeoPixelColors.Black))
 neopixelLight.show()
 basic.showIcon(IconNames.Happy)
 
+input.onButtonPressed(Button.A, function () {
+    neopixelLight.setPixelColor(2, neopixel.colors(NeoPixelColors.Green))
+    neopixelLight.setPixelColor(1, neopixel.colors(NeoPixelColors.Black))
+    neopixelLight.setPixelColor(0, neopixel.colors(NeoPixelColors.Black))
+
+    basic.pause(1000)
+
+    neopixelLight.setPixelColor(2, neopixel.colors(NeoPixelColors.Black))
+    neopixelLight.setPixelColor(1, neopixel.colors(NeoPixelColors.Green))
+    neopixelLight.setPixelColor(1, neopixel.colors(NeoPixelColors.Red))
+    neopixelLight.setPixelColor(0, neopixel.colors(NeoPixelColors.Black))
+
+    basic.pause(1000)
+
+    neopixelLight.setPixelColor(2, neopixel.colors(NeoPixelColors.Black))
+    neopixelLight.setPixelColor(1, neopixel.colors(NeoPixelColors.Black))
+    neopixelLight.setPixelColor(0, neopixel.colors(NeoPixelColors.Red))
+
+    basic.pause(1000)
+})
