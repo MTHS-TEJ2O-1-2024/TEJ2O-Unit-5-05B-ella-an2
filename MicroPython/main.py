8cb621b3cc0b6cb8d95a0b4ce99637ca93830f4a
ella-an2 <ella.an@mths.ca>
2024-11-26 23:18:52 +0000

    tried to code

--- MicroPython/main.py
+++ MicroPython/main.py
@@ -1,10 +1,40 @@
 """
-Created by: Mr. Coxall
-Created on: Sep 2020
+Created by: Ella An
+Created on: Nov 2024
 This module is a Micro:bit MicroPython program
 """
 
 from microbit import *
+import neopixel
+from time import sleep
 
+# setup
+np = neopixel.NeoPixel(pin16, 4)
 
-display.scroll("Hello, World!")
+while True:
+    # turn green on
+    np[0] = (0, 0, 0)
+    np[1] = (0, 0, 0)
+    np[2] = (0, 128, 0)
+    np[3] = (0, 0, 0)
+    np.show()
+
+    sleep(1)
+
+    # turn yello on
+    np[0] = (0, 0, 0)
+    np[1] = (255, 255, 0)
+    np[2] = (0, 0, 0)
+    np[3] = (0, 0, 0)
+    np.show()
+
+    sleep(1)
+
+    # turn red on
+    np[0] = (255, 0, 0)
+    np[1] = (0, 0, 0)
+    np[2] = (0, 0, 0)
+    np[3] = (0, 0, 0)
+    np.show()
+
+    sleep(1)
